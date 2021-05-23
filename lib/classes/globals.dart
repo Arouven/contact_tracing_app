fa17e82c31b2b2542a8e4c483fafa794be2ccbc2
Arouven <aroupoolian@gmail.com>
2021-05-24 02:26:36 +0400

    s

--- lib/classes/globals.dart
+++ lib/classes/globals.dart
@@ -1,11 +1,10 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SP {
-  String fullFilePath;
   main() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    this.fullFilePath = prefs.getString("fullFilePath");
-    prefs.getString("UserName");
+    prefs.getString("fullFilePath");
+    prefs.getString("nationalIdNumber");
     prefs.getString("mobileID");
   }
 }
