b847835eaadf86ad7ca91c9c1c1eef55704444ab
Arouven <aroupoolian@gmail.com>
2021-11-01 18:08:04 +0400

    b

---
+++ lib/my crate app.dart
@@ -0,0 +1,14 @@
+
+
+//todo app
+//reset password
+//live geolocation - on tap markers
+//profile
+
+//todo web
+//datatables
+//map
+//set +ve to virus test
+//set -ve to virus test
+//add testing centres
+
