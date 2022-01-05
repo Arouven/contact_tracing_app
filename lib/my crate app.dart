7fb0737d5a54ee7268132774c0d82a4d81c64fa1
Arouven <aroupoolian@gmail.com>
2022-01-05 16:22:22 +0400

    update todo

--- lib/my crate app.dart
+++ lib/my crate app.dart
@@ -2,13 +2,9 @@
 
 //todo app
 //internet conn
-//live geolocation - on tap markers
 
 
 //todo web
-//datatables
-//map and zoom
-//set +ve to virus test
-//set -ve to virus test
-//add testing centres
+//login for agent
+//index beautify
 
