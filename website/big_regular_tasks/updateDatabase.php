be51569c82f4311ca07c3f1c7e5cccb4270e4a13
Arouven <aroupoolian@gmail.com>
2022-01-11 16:19:55 +0400

    update

--- website/big_regular_tasks/updateDatabase.php
+++ website/big_regular_tasks/updateDatabase.php
@@ -1,56 +1,49 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/database.php';
-//require_once $_SERVER['DOCUMENT_ROOT'] . '/firebase/includes/insert.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/firebase/includes/insert.php';
 
 class updateDatabase
 {
     private $noDaysFromContact;
-    //private $noDaysOfTestValidity;
     private $db;
 
     function __construct()
     {
         $this->db = new database();
         $this->noDaysFromContact = $this->getNumber("daysFromContact"); // 14 -life of virus
-        //  $this->noDaysOfTestValidity = $this->getNumber("daysOfTestValidity"); // 7 - pcr not valid////////////////////
-        //1 +ve - notify 14 days contacts
-        // -ve - 7 days pcr
-        // what ever happen stay -ve for 7 days
 
         $this->deleteOudatedCoordinates();
-        // $this->updateOudatedTests();
-        //echo '<br>';
-        $confirmedInfected = $this->getConfirmedInfected(); //t1=>'lat,lon',t2 ....  
-        //echo '<br>';
-        // print_r($confirmedInfected);
+        $confirmedInfected = $this->getConfirmedInfected(); //t1=>'lat,lon',t2 ...
         $distanceContact = floatval($this->getNumber('contactDistance')); //2
-        //echo '<br>';
-        //print_r($distanceContact);
+
 
         // here space-time is considered as it is absolute
         // distance and coordinates will represent space
         // start with the lowest time (e.g at t1-which represent 14 days in this case)
-        foreach ($confirmedInfected as $key1 => $value1) { //(e.g loop through all infected mobile at t1)
-            $datetimeInfected = $confirmedInfected[$key1]['dateTimeCoordinates'];
-            $mobileNumberInfected = $confirmedInfected[$key1]['mobileNumber'];
-            $latitudeInfected = floatval($confirmedInfected[$key1]['latitude']);
-            $longitudeInfected = floatval($confirmedInfected[$key1]['longitude']);
+        for ($i = 0; $i < count($confirmedInfected); $i++) { //(e.g loop through all infected mobile at t1)
+            $datetimeInfected = $confirmedInfected[$i]['dateTimeCoordinates'];
+            $mobileNumberInfected = $confirmedInfected[$i]['mobileNumber'];
+            $latitudeInfected = floatval($confirmedInfected[$i]['latitude']);
+            $longitudeInfected = floatval($confirmedInfected[$i]['longitude']);
 
 
-
-
-            $nonInfectedAtThatTime = $this->getAllNonInfectedAtThatTime($datetimeInfected); //(e.g get all non infected mobile at t1)
-            foreach ($nonInfectedAtThatTime as $key2 => $value2) { //(e.g loop through all the non infected mobile at t1)
-                $datetimePossibleContact = $nonInfectedAtThatTime[$key2]['dateTimeCoordinates'];
-                $mobileNumberPossibleContact = $nonInfectedAtThatTime[$key2]['mobileNumber'];
-                $latitudePossibleContact = floatval($nonInfectedAtThatTime[$key2]['latitude']);
-                $longitudePossibleContact = floatval($nonInfectedAtThatTime[$key2]['longitude']);
+            $nextdatetimeInfected = $confirmedInfected[$i + 1]['dateTimeCoordinates'];
+            if ($nextdatetimeInfected == null) {
+                $nextdatetimeInfected = $datetimeInfected + 1;
+            }
+            // it is assumed that the mobile stays fixed until the next time from the database
+            $nonInfectedAtThatTime = $this->getAllNonInfectedAtThatTime($datetimeInfected, $nextdatetimeInfected); //(e.g get all non infected mobile at t1 to tx - where tx is the value of next time)
+            for ($j = 0; $j < count($nonInfectedAtThatTime); $j++) { //(e.g loop through all the non infected mobile at t1)
+                $datetimePossibleContact = $nonInfectedAtThatTime[$j]['dateTimeCoordinates'];
+                $mobileNumberPossibleContact = $nonInfectedAtThatTime[$j]['mobileNumber'];
+                $latitudePossibleContact = floatval($nonInfectedAtThatTime[$j]['latitude']);
+                $longitudePossibleContact = floatval($nonInfectedAtThatTime[$j]['longitude']);
                 $distanceMetres = $this->distanceMetres($latitudeInfected, $longitudeInfected, $latitudePossibleContact, $longitudePossibleContact); //(e.g get the distance between the infected mobile and the non infected mobile at t1)
                 if ($distanceMetres <= $distanceContact) { //(e.g if the distance between them is less or equal to 2 meters)
                     //mark contact with infected
                     print "$mobileNumberPossibleContact will be marked as contact, infected by $mobileNumberInfected because distance between them is $distanceMetres meters at $datetimePossibleContact.<br>";
                     //send message to mobile
-                    //  new NotifyFirebase($mobileNumberPossibleContact);
+                    new NotifyFirebase($mobileNumberPossibleContact);
                     print "message sent to $mobileNumberPossibleContact <br>";
                     $this->markAsContact($mobileNumberPossibleContact);
                 } else {
@@ -64,10 +57,10 @@ class updateDatabase
         $updateStatement = "UPDATE Mobile SET contactWithInfected = TRUE WHERE mobileNumber = $mobileNumberContact;";
         $this->db->execute($updateStatement);
     }
-    function getAllNonInfectedAtThatTime($datetimeInfected)
+    function getAllNonInfectedAtThatTime($startdatetimeInfected, $enddatetimeInfected)
     {
         //  $selectquery = "SELECT Coordinates.dateTimeCoordinates, Coordinates.latitude, Coordinates.longitude, Mobile.mobileNumber, Mobile.mobileNumber FROM Coordinates INNER JOIN Mobile ON Coordinates.mobileNumber = Mobile.mobileNumber WHERE Mobile.confirmInfected = FALSE AND Coordinates.dateTimeCoordinates = $datetimeInfected AND Mobile.dateTimeLastTest IS NULL;";
-        $selectquery = "SELECT Coordinates.dateTimeCoordinates, Coordinates.latitude, Coordinates.longitude, Mobile.mobileNumber, Mobile.mobileNumber FROM Coordinates INNER JOIN Mobile ON Coordinates.mobileNumber = Mobile.mobileNumber WHERE Mobile.confirmInfected = FALSE AND Coordinates.dateTimeCoordinates = $datetimeInfected AND Mobile.contactWithInfected = FALSE;";
+        $selectquery = "SELECT Coordinates.dateTimeCoordinates, Coordinates.latitude, Coordinates.longitude, Mobile.mobileNumber, Mobile.mobileNumber FROM Coordinates INNER JOIN Mobile ON Coordinates.mobileNumber = Mobile.mobileNumber WHERE Mobile.confirmInfected = FALSE AND Mobile.contactWithInfected = FALSE AND Coordinates.dateTimeCoordinates >= $startdatetimeInfected AND Coordinates.dateTimeCoordinates < $enddatetimeInfected;";
         $data = $this->db->select($selectquery);
         $array = array();
 
