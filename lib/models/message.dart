6a729576c64a2fb8ab23b07a5015c6f7ab341e92
Arouven <aroupoolian@gmail.com>
2022-01-21 16:12:35 +0400

    add animation

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_database/firebase_database.dart';
+
 class Message {
   Message({
     required this.id,
@@ -12,4 +14,15 @@ class Message {
   String body;
   bool read;
   int timestamp;
+
+  factory Message.fromRTDB(DataSnapshot snapshot) {
+    var map = Map<String, dynamic>.from(snapshot.value as Map);
+    return Message(
+      id: snapshot.key ?? '0',
+      title: map['title'] as String,
+      body: map['body'] as String,
+      read: map['read'] as bool,
+      timestamp: map['timestamp'] as int,
+    );
+  }
 }
